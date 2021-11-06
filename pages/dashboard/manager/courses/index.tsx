56c93be1dc93d97fdae4c855c6cf91a34c537059
yangpengchao1 <yangpengchao1@gmail.com>
2021-11-06 21:50:37 +1300

    一直在抓取数据，置顶样式还是未显示

--- pages/dashboard/manager/courses/index.tsx
+++ pages/dashboard/manager/courses/index.tsx
@@ -1,7 +1,7 @@
 import AppLayout from "../../../../components/layout/AppLayout";
-import {useEffect, useState} from "react";
-import {BackTop, Button, Card, Col, Divider, List, Row, Skeleton} from 'antd';
-import InfiniteScroll from 'react-infinite-scroll-component';
+import {useCallback, useEffect, useState} from "react";
+import {BackTop, Button, Card, Col, Divider, List, Row, Skeleton} from "antd";
+import InfiniteScroll from "react-infinite-scroll-component";
 import {AxiosResponse} from "axios";
 import {BaseResponse} from "../../../../libs/Entity/response/BaseResponse";
 import {courseAPI} from "../../../../libs/API/CourseAPI";
@@ -10,143 +10,137 @@ import {Course} from "../../../../libs/Entity/Course";
 import {GetCoursesResponse} from "../../../../libs/Entity/response/GetCoursesResponse";
 import {HeartFilled, UserOutlined} from "@ant-design/icons";
 
-
 export default function Courses() {
-
     const [loading, setLoading] = useState(false);
     const [data, setData] = useState<Course[]>([]);
     const [page, setPage] = useState<number>(1);
-    const [limit, setLimit] = useState<number>(20);
+    const [total, setTotal] = useState<number>(0);
+    const limit = 20;
 
-    const loadMoreData = async () => {
+    const loadMoreData = useCallback(async () => {
         if (loading) {
             return;
         }
         setLoading(true);
         const getCoursesRequest = new GetCoursesRequest(page, limit);
-        const resp: AxiosResponse<BaseResponse<GetCoursesResponse>> = await courseAPI.getCourseList(getCoursesRequest);
-        console.log(resp.data.data.courses[0].cover);
-        // @ts-ignore
-        setData(resp.data.data.courses);
-        // setData([...data, resp.data.data.courses]);
+        const resp: AxiosResponse<BaseResponse<GetCoursesResponse>> =
+            await courseAPI.getCourseList(getCoursesRequest);
+        console.log(resp.data.data.courses);
+        setData([...data, ...resp.data.data.courses]);
+        setTotal(resp.data.data.total);
         setLoading(false);
-    };
+    }, [data, loading, page]);
 
     useEffect(() => {
         (async () => {
             await loadMoreData();
         })();
-    }, [page, limit]);
+    }, [loadMoreData]);
 
-    return (<AppLayout>
-        <div
-            id="scrollableDiv"
-            style={{
-                height: 'auto',
-                overflow: 'auto',
-                padding: '0',
-                border: '1px solid rgba(140, 140, 140, 0.35)',
-            }}
-        >
-            <InfiniteScroll
-                dataLength={data.length}
-                next={loadMoreData}
-                hasMore={data.length < 20}
-                loader={<Skeleton avatar paragraph={{rows: 1}} active/>}
-                endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
-                scrollableTarget="scrollableDiv"
+    return (
+        <AppLayout>
+            <div
+                id="scrollableDiv"
+                style={{
+                    height: "auto",
+                    overflow: "auto",
+                    padding: "0",
+                    border: "1px solid rgba(140, 140, 140, 0.35)",
+                }}
             >
-                <List
-                    grid={{gutter: 16, column: 4}}
-                    dataSource={data}
-                    renderItem={item => (
-                        <List.Item key={item.id} style={{margin: '8 6'}}>
-                            <Card
-                                style={{"height": "100%"}}
-                                cover={<img style={{height: 260}}
-                                            src={item.cover}
-                                />}
-                            >
-                                <Row gutter={[6, 16]}>
-                                    <h3>{item.name}</h3>
-                                </Row>
+                <InfiniteScroll
+                    dataLength={data.length}
+                    next={() => setPage(page + 1)}
+                    hasMore={data.length < total}
+                    loader={<Skeleton avatar paragraph={{rows: 1}} active/>}
+                    endMessage={<Divider plain>It is all, nothing more 🤐</Divider>}
+                    scrollableTarget="scrollableDiv"
+                >
+                    <List
+                        grid={{gutter: 16, column: 4}}
+                        dataSource={data}
+                        renderItem={(item) => (
+                            <List.Item key={item.id} style={{margin: "8 6"}}>
+                                <Card
+                                    style={{height: "100%"}}
+                                    cover={<img style={{height: 260}} src={item.cover}/>}
+                                >
+                                    <Row gutter={[6, 16]}>
+                                        <h3>{item.name}</h3>
+                                    </Row>
 
-                                <Row gutter={[16, 16]} style={{}}>
-                                    <Col span={12}>{item.createdAt}</Col>
-                                    <Col span={12}>
-                                        <div style={{float: 'right'}}>
-                                            <HeartFilled style={{marginRight: 5, fontSize: 16, color: 'red'}}/>
-                                            {item.star}
-                                        </div>
-                                    </Col>
+                                    <Row gutter={[16, 16]} style={{}}>
+                                        <Col span={12}>{item.createdAt}</Col>
+                                        <Col span={12}>
+                                            <div style={{float: "right"}}>
+                                                <HeartFilled
+                                                    style={{marginRight: 5, fontSize: 16, color: "red"}}
+                                                />
+                                                {item.star}
+                                            </div>
+                                        </Col>
 
-                                    <Col span={12}>Duration:</Col>
-                                    <Col span={12}>
-                                        <div style={{float: 'right'}}>
-                                            <b>{item.duration} years</b>
-                                        </div>
-                                    </Col>
+                                        <Col span={12}>Duration:</Col>
+                                        <Col span={12}>
+                                            <div style={{float: "right"}}>
+                                                <b>{item.duration} years</b>
+                                            </div>
+                                        </Col>
 
-                                    <Col span={12}>Teacher:</Col>
-                                    <Col span={12}>
-                                        <div style={{float: 'right'}}>
-                                            <a><b>{item.teacherName}</b></a>
-                                        </div>
-                                    </Col>
+                                        <Col span={12}>Teacher:</Col>
+                                        <Col span={12}>
+                                            <div style={{float: "right"}}>
+                                                <a>
+                                                    <b>{item.teacherName}</b>
+                                                </a>
+                                            </div>
+                                        </Col>
 
-                                    <Col span={12}><UserOutlined
-                                        style={{color: '#0086f9', fontSize: '16px', marginRight: '5px'}}/>Student Limit:</Col>
-                                    <Col span={12}>
-                                        <div style={{float: 'right'}}>
-                                            <b>{item.maxStudents}</b>
-                                        </div>
-                                    </Col>
-                                    <Col span={24}>
-                                        <Button type="primary">Read More</Button>
-                                    </Col>
-                                </Row>
-                            </Card>
-                        </List.Item>
+                                        <Col span={12}>
+                                            <UserOutlined
+                                                style={{
+                                                    color: "#0086f9",
+                                                    fontSize: "16px",
+                                                    marginRight: "5px",
+                                                }}
+                                            />
+                                            Student Limit:
+                                        </Col>
+                                        <Col span={12}>
+                                            <div style={{float: "right"}}>
+                                                <b>{item.maxStudents}</b>
+                                            </div>
+                                        </Col>
+                                        <Col span={24}>
+                                            <Button type="primary">Read More</Button>
+                                        </Col>
+                                    </Row>
+                                </Card>
+                            </List.Item>
+                        )}
+                    />
+                </InfiniteScroll>
 
-                    )}
-                />
-            </InfiniteScroll>
-        </div>
-        {/*<BackTop visibilityHeight={20}>*/}
-        {/*    <div style={{*/}
-        {/*        height: 40,*/}
-        {/*        width: 40,*/}
-        {/*        lineHeight: '40px',*/}
-        {/*        borderRadius: 4,*/}
-        {/*        backgroundColor: '#1088e9',*/}
-        {/*        color: '#fff',*/}
-        {/*        textAlign: 'center',*/}
-        {/*        fontSize: 14,*/}
-        {/*    }}>UP*/}
-        {/*    </div>*/}
-        {/*</BackTop>*/}
-
-        {/*<div style={{ height: '600vh', padding: 8 }}>*/}
-        {/*    <div>Scroll to bottom</div>*/}
-        {/*    <div>Scroll to bottom</div>*/}
-        {/*    <div>Scroll to bottom</div>*/}
-        {/*    <div>Scroll to bottom</div>*/}
-        {/*    <div>Scroll to bottom</div>*/}
-        {/*    <div>Scroll to bottom</div>*/}
-        {/*    <div>Scroll to bottom</div>*/}
-        {/*    <BackTop>*/}
-        {/*        <div style={{*/}
-        {/*            height: 40,*/}
-        {/*            width: 40,*/}
-        {/*            lineHeight: '40px',*/}
-        {/*            borderRadius: 4,*/}
-        {/*            backgroundColor: '#1088e9',*/}
-        {/*            color: '#fff',*/}
-        {/*            textAlign: 'center',*/}
-        {/*            fontSize: 14,*/}
-        {/*        }}>UP*/}
-        {/*        </div>*/}
-        {/*    </BackTop>*/}
-        {/*</div>*/}
-    </AppLayout>)
+                <BackTop
+                    visibilityHeight={20}
+                    target={() => document.getElementById("scrollableDiv")}>
+                    <div
+                        style={{
+                            height: 40,
+                            width: 40,
+                            lineHeight: "40px",
+                            borderRadius: 4,
+                            backgroundColor: "#1088e9",
+                            color: "#fff",
+                            textAlign: "center",
+                            fontSize: 14,
+                        }}
+                    >
+                        UP
+                    </div>
+                    {/*<div>UP</div>*/}
+                </BackTop>
+            </div>
+        </AppLayout>
+    );
 }
---
+++ pages/dashboard/manager/courses/test.tsx
@@ -0,0 +1,25 @@
+import {BackTop} from "antd";
+
+export default function test(){
+    return (
+        <div style={{ height: '600vh', padding: 8 }}>
+            <div>Scroll to bottom</div>
+            <div>Scroll to bottom</div>
+            <div>Scroll to bottom</div>
+            <div>Scroll to bottom</div>
+            <div>Scroll to bottom</div>
+            <div>Scroll to bottom</div>
+            <div>Scroll to bottom</div>
+            <BackTop>
+                <div style={{height: 40,
+                    width: 40,
+                    lineHeight: '40px',
+                    borderRadius: 4,
+                    backgroundColor: '#1088e9',
+                    color: '#fff',
+                    textAlign: 'center',
+                    fontSize: 14,}}>UP</div>
+            </BackTop>
+        </div>
+    )
+}
